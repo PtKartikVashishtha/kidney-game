bb43e39a8bf18b50e1af0c25858ddf1a37027f1f
KARTIK VASHISHTHA <kartik2005vashishtha@gmail.com>
2024-11-25 21:21:57 +0530

    Update hospital.js

--- hospital.js
+++ hospital.js
@@ -2,7 +2,7 @@ const express = require("express") ;
 let port = 3000 ;
 const app = express() ;
 let data = [{
-    name : "jhon",
+    name : "john",
     kidneys : [{
         condition : "good" ,
     } , {
